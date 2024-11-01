8917146b82664c5b64bb325873429b2fb6411553
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-01 23:07:03 +0600

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,7 @@ import autoprefixer from 'autoprefixer';
 
 export default {
   plugins: [
-    tailwindcss('./tailwind.config.cjs'),
+    tailwindcss('./tailwind.config.js'),
     autoprefixer,
   ],
 };

ba76dc05a747b56f9e90de158312c1e359e4d9fc
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-01 22:47:05 +0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    autoprefixer,
+  ],
+};

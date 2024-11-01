90c80454f11c1ae492254e5a8fc53e1c2a5436f9
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-01 22:30:09 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Import your global styles
+import './index.css';
 
-// Create a root element for React to render into
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Render the App component
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
